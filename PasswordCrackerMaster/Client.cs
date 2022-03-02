156bc184f5498eec6d7083819ec5b42a3ab6e205
Sherifsss <kristaps_mezatucs2@inbox.lv>
2022-03-02 12:36:50 +0100

    Commit

---
+++ PasswordCrackerMaster/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerMaster
+{
+    public class Client
+    {
+        public string IPAdrress { get; set; }
+        public bool Ready { get; set; }
+        public List<string> Chunk { get; set; }
+    }
+}
