6b0ca0d499adafa6365bedfdb5f1f0168f11309e
Sherifsss <kristaps_mezatucs2@inbox.lv>
2022-03-02 10:21:17 +0100

    Add project files.

---
+++ PasswordCrackerMaster/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerMaster
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Loopback, 10001);
+            listener.Start();
+            Console.WriteLine("Master is here");
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task.Run(() =>
+                {
+                    HandleClient(socket);
+                });
+            }
+        }
+
+
+
+        public static void HandleClient()
+        {
+
+        }
+    }
+}
